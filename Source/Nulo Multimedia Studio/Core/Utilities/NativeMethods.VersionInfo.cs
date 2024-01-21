b7c347ec67f850ec57264d5498fc9260ec002e9d
Kastney Frazão <kastney@outlook.com>
2024-01-21 14:48:41 -0300

    ✨ feat - Leaving the application as a single instance

---
+++ Source/Nulo Multimedia Studio/Core/Utilities/NativeMethods.VersionInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace Nulo.Core.Utilities {
+
+    internal static partial class NativeMethods {
+
+        public static string GetCopyright() {
+            try {
+                var info = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+                return info.LegalCopyright;
+            } catch {
+                return string.Empty;
+            }
+        }
+
+        public static string GetSmallVersion() {
+            try {
+                var info = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+                return $"{info.ProductMajorPart}.{info.ProductMinorPart}";
+            } catch {
+                return string.Empty;
+            }
+        }
+
+        public static string GetProductName() {
+            try {
+                var info = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+                return info.ProductName.Replace(" ®", "®");
+            } catch {
+                return string.Empty;
+            }
+        }
+
+        public static string GetIdProduct() {
+            return "{e4ef5f49-81d8-4933-b491-e81ae84f9bd6}";
+        }
+    }
+}
